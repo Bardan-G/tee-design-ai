b4c20131164a513a8f7e88602808829d1ec4b3ee
Bardan-g <neupanebardang@gmail.com>
2025-08-17 17:59:28 +0545

    creating Customizer section

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,16 @@
+import { useState,useEffect } from "react";
+import { AnimatePresence,motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from '../config/config';
+import state from "../store";
+import {download} from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AiPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../comopnents';
 import React from 'react'
+
+
 
 const Customizer = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import {motion,AnimatePresence} from 'framer-motion';
-import {useSnapshot} from 'valtio';
-import state from '../store';
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { CustomButton } from "../comopnents";
 import {
   headContainerAnimation,
   headContentAnimation,
   headTextAnimation,
-  slideAnimation
-}from '../config/motion'
+  slideAnimation,
+} from "../config/motion";
 const Home = () => {
   const snap = useSnapshot(state);
   return (
     <AnimatePresence>
       {snap.intro && (
-        <motion.section className='home'{...slideAnimation('left')}>
-          <motion.header {...slideAnimation('down')}>
-            <img src="./threejs.png" alt="logo" className='w-8 h-8 object-contain' />
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
           </motion.header>
 
-          <motion.div className='home-contain'{...headContainerAnimation}>
-            
+          <motion.div className="home-contain" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
-              <h1 className='head-text'>LET'S <br className='xl:block hidden' />DO IT</h1>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" />
+                DO IT
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Design your own shirt with our interactive 3D customization
+                tool. <strong>Bring your vision to life</strong> and create a
+                style that’s truly yours.
+              </p>
+              <CustomButton
+              type="filled"
+              title="Customize it"
+              handleClick={()=>state.intro = false}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm '
+              />
             </motion.div>
           </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
